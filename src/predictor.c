6db711e3b03c38a283dad55e68a66886275af018
Zhiyuan Qi <qizhyuan@gmail.com>
2022-12-05 17:28:30 -0800

    test gshare

--- src/predictor.c
+++ src/predictor.c
@@ -36,6 +36,96 @@ int verbose;
 //
 //TODO: Add your own Branch Predictor data structures here
 //
+// Get the bit-th bit of x
+#define GET_BIT(x, bit) ((x >> bit) & 1)
+
+// Get the lower k bits of x
+#define GET_LOWER_K_BITS(x, k) (x & ((1 << k) - 1))
+
+// Set the bit-th bit of x into y
+#define SET_BIT(x, bit, y) ((x & ~ (1 << bit)) | (y << bit))
+
+uint32_t globalHistory;
+uint8_t* twoBitsCounters;
+
+
+uint32_t get_index(uint32_t pc) {
+  return globalHistory ^ pc;
+}
+
+uint8_t get_counter(uint32_t index) {
+  uint32_t array_index = index >> 2;
+  uint32_t item_index = index & 0b11;
+  
+  uint32_t item = twoBitsCounters[array_index];
+  uint8_t counter = GET_BIT(item, item_index << 1) | GET_BIT(item, item_index << 1 + 1);
+  return counter;
+}
+
+void set_counter(uint32_t index, uint8_t counter) {
+  uint32_t array_index = index >> 2;
+  uint32_t item_index = index & 0b11;
+  uint32_t item = twoBitsCounters[array_index];
+  
+  item = SET_BIT(item, item_index << 1, counter & 0b01);
+  item = SET_BIT(item, item_index << 1 + 1, counter & 0b10);
+
+  twoBitsCounters[array_index] = item;
+}
+
+void decrease_counter(uint32_t index) {
+  uint8_t counter = get_counter(index);
+  uint8_t new_counter = 0;
+  if (counter == SN) {
+    new_counter = SN;
+  } else if (counter == WN) {
+    new_counter = SN;
+  } else if (counter == WT) {
+    new_counter = WN;
+  } else {
+    new_counter = WT;
+  }
+  set_counter(index, new_counter);
+}
+
+void increase_counter(uint32_t index) {
+  uint8_t counter = get_counter(index);
+  uint8_t new_counter = 0;
+  if (counter == SN) {
+    new_counter = WN;
+  } else if (counter == WN) {
+    new_counter = WT;
+  } else if (counter == WT) {
+    new_counter = ST;
+  } else {
+    new_counter = ST;
+  }
+  set_counter(index, new_counter);
+}
+
+uint8_t gshare_predictor(uint32_t pc) {
+  uint32_t index = get_index(pc);
+  uint8_t counter = get_counter(index);
+  if (counter == SN || counter == WN) {
+    return NOTTAKEN;
+  } else {
+    return TAKEN;
+  }
+}
+
+uint8_t gshare_trainer(uint32_t pc, uint8_t outcome) {
+  uint32_t index = get_index(pc);
+
+  if (outcome == NOTTAKEN) {
+    decrease_counter(index);
+    globalHistory = globalHistory << 1 | 1;
+  } else {
+    increase_counter(index);
+    globalHistory = globalHistory >> 1;
+  }
+
+  globalHistory = GET_LOWER_K_BITS(globalHistory, ghistoryBits);
+}
 
 
 //------------------------------------//
@@ -68,6 +158,7 @@ make_prediction(uint32_t pc)
     case STATIC:
       return TAKEN;
     case GSHARE:
+      return gshare_predictor(pc);
     case TOURNAMENT:
     case CUSTOM:
     default:
@@ -88,4 +179,13 @@ train_predictor(uint32_t pc, uint8_t outcome)
   //
   //TODO: Implement Predictor training
   //
+    switch (bpType) {
+    case STATIC:
+    case GSHARE:
+      gshare_trainer(pc, outcome);
+    case TOURNAMENT:
+    case CUSTOM:
+    default:
+      break;
+  }
 }
