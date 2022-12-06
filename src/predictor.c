f574502e6a5b1bd10e7ab90e1a8dc432f0d3ce98
Zhiyuan Qi <qizhyuan@gmail.com>
2022-12-05 18:45:05 -0800

    fix switch bug

--- src/predictor.c
+++ src/predictor.c
@@ -61,7 +61,7 @@ uint8_t get_counter(uint32_t index) {
   uint32_t item_index = index & 0b11;
   
   uint8_t item = twoBitsCounters[array_index];
-  uint8_t counter = GET_BIT(item, item_index << 1) | GET_BIT(item, item_index << 1 + 1);
+  uint8_t counter = GET_BIT(item, (item_index << 1)) | GET_BIT(item, ((item_index << 1) + 1));
   return counter;
 }
 
@@ -70,8 +70,8 @@ void set_counter(uint32_t index, uint8_t counter) {
   uint32_t item_index = index & 0b11;
   uint8_t item = twoBitsCounters[array_index];
   
-  item = SET_BIT(item, item_index << 1, counter & 0b01);
-  item = SET_BIT(item, item_index << 1 + 1, counter & 0b10);
+  item = SET_BIT(item, (item_index << 1), (counter & 0b01));
+  item = SET_BIT(item, ((item_index << 1) + 1), (counter & 0b10));
 
   twoBitsCounters[array_index] = item;
 }
